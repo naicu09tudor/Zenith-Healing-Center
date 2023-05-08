511bc6cf39fccd57ba8c8c72e6b24f62478ac955
naicu-tudor-30121 <naicutudor@yahoo.com>
2023-05-08 21:06:05 +0300

    Add project files.

---
+++ ZenithHealingCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZenithHealingCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
