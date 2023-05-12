b2fb32d353fdf807c71061a297f4da62339726dd
naicu-tudor-30121 <naicutudor@yahoo.com>
2023-05-12 13:30:11 +0300

    Update 2000 - Azure DB added Backend is done

--- ZenithHealingCenter/Data/AppDbInitializer.cs
+++ ZenithHealingCenter/Data/AppDbInitializer.cs
@@ -22,16 +22,35 @@ namespace ZenithHealingCenter.Data
                         new Doctor()
                         {
                             FullName = "Naicu Tudor",
-                            SpecializareDoctor = Enums.Specializare.Ginecologie,
-                            ImageURL = "C:\\Users\\naicu\\Pictures\\Poze\\12A-745copy3.jpg"
+                            SpecializareDoctor = Enums.Specializare.Oftalmologie,
+                            ImageURL = "https://t4.ftcdn.net/jpg/03/05/41/27/360_F_305412791_XRNiWaFCREjLLpSQfj0e736foBoYXXYv.jpg"
                         },
 
                          new Doctor()
                         {
                             FullName = "Voina Sebastian",
                             SpecializareDoctor = Enums.Specializare.Ginecologie,
-                            ImageURL = "C:\\Users\\naicu\\Downloads\\WhatsApp Image 2023-05-09 at 19.32.33.jpeg"
+                            ImageURL = "https://t4.ftcdn.net/jpg/03/05/41/27/360_F_305412791_XRNiWaFCREjLLpSQfj0e736foBoYXXYv.jpg"
                         },
+
+                         new Doctor()
+                        {
+                            FullName = "Popa Ioana",
+                            SpecializareDoctor = Enums.Specializare.Cardiologie,
+                            ImageURL = "https://img.freepik.com/free-photo/beautiful-young-female-doctor-looking-camera-office_1301-7807.jpg?w=2000"
+                        },
+                         new Doctor()
+                        {
+                            FullName = "Rizel Catalina",
+                            SpecializareDoctor = Enums.Specializare.Dermatologie,
+                            ImageURL = "https://img.freepik.com/free-photo/beautiful-young-female-doctor-looking-camera-office_1301-7807.jpg?w=2000"
+                        },
+                         new Doctor()
+                         {
+                             FullName = "Dr. Haus",
+                             SpecializareDoctor = Enums.Specializare.Diabetologie,
+                             ImageURL = "https://cdn.wallpapersafari.com/63/37/vqmB61.jpg"
+                         }
 
                     });
                     context.SaveChanges();
@@ -44,8 +63,34 @@ namespace ZenithHealingCenter.Data
                     {
                         new Cabinet()
                         {
-                            Name = "Cabinetul Ginecologilor de alaturi",
-                            Description = "Sala 305 B",
+                            Name = "Cabinet Ginecologie",
+                            Description = "Sala 304",
+
+                        },
+
+                        new Cabinet()
+                        {
+                            Name = "Cabinet Dermatologie",
+                            Description = "Sala 303",
+
+                        },
+                        new Cabinet()
+                        {
+                            Name = "Cabinet Cardiologie",
+                            Description = "Sala 302",
+
+                        },
+                        new Cabinet()
+                        {
+                            Name = "Cabinet Diabetologie",
+                            Description = "Sala 301",
+
+                        },
+
+                        new Cabinet()
+                        {
+                            Name = "Cabinet Oftalmologie",
+                            Description = "Sala 305",
 
                         }
                     });
@@ -60,13 +105,49 @@ namespace ZenithHealingCenter.Data
                         new MedicalPackage()
                         {
                             Name = "Pachet sreening diabet",
-                            Description = "Consult mdeicina generala/interna; Glucoza serica(glicemie)",
+                            Description = "Consult medicina generala/interna" +
+                            "Glucoza serica(glicemie)",
                             Price = 280,
-                            ImageFile = "C:\\Users\\naicu\\Desktop\\II\\Proiect\\Screenshot 2023-05-09 193550.jpg",
+                            ImageFile = "https://smartliving.ro/wp-content/uploads/2020/01/glicemia-mare-cauze-manifestari-tratament-scaled.jpg",
                             SpecializarePachet = Enums.Specializare.Diabetologie,
+                            CabinetId = 4,
+                            DoctorId = 5
+                        },
+
+                        new MedicalPackage()
+                        {
+                            Name = "Pachet sreening cardiovascular",
+                            Description = "Consult medicina generala/interna " +
+                            "Profil lipidic(LDC colesterol, HDL colesterol, colesterol total, trigliceride)",
+                            Price = 360,
+                            ImageFile = "https://img.freepik.com/free-vector/cardiology-clinic-hospital-department-healthy-heart-cardiovascular-prevention-healthcare-industry-idea-design-element-electrocardiogram-ekg-vector-isolated-concept-metaphor-illustration_335657-1516.jpg?w=2000",
+                            SpecializarePachet = Enums.Specializare.Cardiologie,
+                            CabinetId = 3,
+                            DoctorId = 3
+                        },
+
+                        new MedicalPackage()
+                        {
+                            Name = "Pachet sreening cancer de piele",
+                            Description = "Consult dermatologie" +
+                            "Dermatoscopie (fara inregitrare date)",
+                            Price = 200,
+                            ImageFile = "https://img.freepik.com/free-vector/tiny-dermatologist-examining-dry-face-skin-flat-vector-illustration-abstract-epidermis-disease-diagnostics-treatment-dermatology-health-medical-protection-cosmetology-concept_74855-10167.jpg",
+                            SpecializarePachet = Enums.Specializare.Dermatologie,
                             CabinetId = 2,
-                            DoctorId = 2
-                        }
+                            DoctorId = 4
+                        },
+
+                        new MedicalPackage()
+                        {
+                            Name = "Pachet consult oftalmologic",
+                            Description = "Consult oftalmologic: Autorefractometrie, Determinarea acuitatii vizuale, Masurarea tensiniunii intraoculare, Biomicroscopia, Examinarea fundului de ochi",
+                            Price = 410,
+                            ImageFile = "https://thumbs.dreamstime.com/b/ophthalmology-web-banner-page-tiny-cartoon-people-ophthalmology-medicine-optical-eyesight-examination-flat-vector-158504291.jpg",
+                            SpecializarePachet = Enums.Specializare.Oftalmologie,
+                            CabinetId = 5,
+                            DoctorId = 1
+                        },
                     }) ;
                     context.SaveChanges();
                 }
